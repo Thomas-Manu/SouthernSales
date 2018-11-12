c23818dbbe25d60b2d564a4dd745992907d0f8b5
Thomas M <tomkm22@gmail.com>
2018-11-11 22:28:28 -0500

    Initial Commit

---
+++ SouthernSales/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SouthernSales
+//
+//  Created by Thomas Manu on 11/11/18.
+//  Copyright © 2018 Thomas Manu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
