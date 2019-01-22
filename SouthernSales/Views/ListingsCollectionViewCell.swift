5a347a4b34e70ae1b9de1d2996b65a268f1bb2fb
Thomas M <tomkm22@gmail.com>
2019-01-21 22:11:12 -0500

    Fix border issues

--- SouthernSales/Views/ListingsCollectionViewCell.swift
+++ SouthernSales/Views/ListingsCollectionViewCell.swift
@@ -16,18 +16,23 @@ class ListingsCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+}
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
-        contentView.layer.cornerRadius = 3
+        contentView.layer.cornerRadius = 5
         contentView.layer.borderWidth = 0.5
         contentView.layer.borderColor = UIColor.clear.cgColor
         contentView.layer.masksToBounds = true
         
         layer.shadowColor = UIColor.lightGray.cgColor
-        layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        layer.shadowOffset = CGSize(width: 5, height: 5)
         layer.shadowRadius = 3
         layer.shadowOpacity = 0.5
         layer.masksToBounds = false
         layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+
     }
     
     func configure(title: String, price: Double) {
